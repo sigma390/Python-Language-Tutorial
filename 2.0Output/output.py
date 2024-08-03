717fb5fff5a8dcf5cee5358f7b63566b76eebe22
Omkar Patil <git21cs072-t@students.git.edu>
2024-08-03 20:44:19 +0530

    print and output Done!!!

---
+++ 2.0Output/output.py
@@ -0,0 +1,12 @@
+
+
+
+#Printing Multiple Data At once 
+#each , is a Space in Output Line
+
+print(123, "Hi", False, 2.22);
+
+
+#specify custom end as Default end = "\n";
+
+print("Hellowpppp",end='\w');
