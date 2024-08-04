7b1aac45707f75270e74c047854b00bccfb0826d
Omkar Patil <git21cs072-t@students.git.edu>
2024-08-04 15:12:15 +0530

    Airthmatic Operations Done!!!

---
+++ 05.Airthmetic Operators/Ao.py
@@ -0,0 +1,18 @@
+# + - * ^ How to perform MaTHEMARTICAL Operations in python
+
+x = 10
+y = 12
+add = x+y
+sub = x-y
+mul = x*y
+div = x/y #flaot
+divv = x//y #int 
+mod = x%y #remainder
+expo = x^y
+print(add , sub , mul , div , divv , mod, expo);
+
+#type Conversion 
+num1 = int(input("Enter Number1 :"))
+num2 = int(input("Enter Number2 :"))
+res = num1+num2;
+print(res)
